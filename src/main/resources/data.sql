ca5b2995208db359fd6af385d3b54d3f704d3c61
vivekbhardwaj17 <vivekofficial2018@gmail.com>
2023-12-01 18:25:54 +0530

    added h2 db and manual db entry by data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO userdetails (id, name) VALUES (1, 'Sagar');
+INSERT INTO userdetails (id, name) VALUES (2, 'Vivek');
+INSERT INTO userdetails (id, name) VALUES (3, 'Pushpendra');
+INSERT INTO userdetails (id, name) VALUES (4, 'Rupendra');
+INSERT INTO userdetails (id, name) VALUES (5, 'Group');
